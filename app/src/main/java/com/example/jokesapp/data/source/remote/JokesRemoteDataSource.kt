003cbec54a76fb1d35f4bf73965f3b060b16a5b3
Ferrian Redhia Pratama <ferrianrp@gmail.com>
2021-06-22 02:10:13 +0700

    data source buat ngambil ke api udah aman

---
+++ app/src/main/java/com/example/jokesapp/data/source/remote/JokesRemoteDataSource.kt
@@ -0,0 +1,34 @@
+package com.example.jokesapp.data.source.remote
+
+import com.example.jokesapp.data.source.remote.network.ApiResponse
+import com.example.jokesapp.data.source.remote.network.ApiService
+import com.example.jokesapp.data.source.remote.response.ResponseJokesItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.lang.Exception
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class JokesRemoteDataSource @Inject constructor(private val apiService: ApiService){
+
+    suspend fun getAllJokes(): Flow<ApiResponse<List<ResponseJokesItem?>>> {
+        return flow {
+            try {
+                val response = apiService.getAllJokes()
+                val dataJokes = response.responseJokes
+                if (dataJokes != null){
+                    if (dataJokes.isNotEmpty())
+                        emit(ApiResponse.Success(dataJokes))
+                    else
+                        emit(ApiResponse.Empty)
+                }
+            } catch (e: Exception){
+                emit(ApiResponse.Error(e.message.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
